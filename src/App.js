96af6760d5ecb7ed076482902feb48cdd93e7c5a
JoseGonzalez098 <gon20335@uvg.edu.gt>
2022-06-03 23:56:45 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,88 +1,186 @@
 import React, {useState } from 'react';
-import operacion from './operaciones';
 import './App.css';
 
 const App = () => {
 
-  const [nega, setNega] = useState(false);
-
-  const [resul, setResul] = useState("");
+  const [size, setSize] = useState("");
+  const [font, setFont] = useState("");
+  const [back, setBack] = useState("");
+  // const [resul, setResul] = useState("");
 
   const handleClick = (e) => {
     console.log(e)
-    setResul(resul.concat(e.target.name));
-  }
-
-  const clear = () => {
-    setResul("");
-  }
-
-  const borrar = () => {
-    setResul(resul.slice(0,resul.length-1));
-  }
-
-  const calcularClick = () => {
-
-    let numero1 = resul;
-    let verdaderofalso = nega;
-
-    console.log("-*********-*********-******-entro",numero1,verdaderofalso)
-
-    let valor = operacion(numero1,verdaderofalso);
-
-    setNega(false);
-    setResul(valor);
-  }
-
-  const negativador = () => {
-
-    setNega(true);
-
-    let valor = resul
-
-    console.log("entro")
-
-    if(resul > 0){
-      console.log("entro if")
-      valor = "-" + resul
-    } else{
-      console.log("entro else")
-      valor.substring(1);
-    }
-
-    console.log(valor);
-    setResul(valor)
-
   }
 
   return (
-    <div className="calcu">
-      <form>
-        <input type="text" value={resul} />
-      </form>
-      <div className='teclado'>
-        <button className='especial' onClick={clear}>Clear</button>
-        <button className='botonColorido' onClick={borrar}>C</button>
-        <button className='botonColorido' name='/' onClick={handleClick}>/</button>
-        <button className='boton' name='7' onClick={handleClick}>7</button>
-        <button className='boton' name='8' onClick={handleClick}>8</button>
-        <button className='boton' name='9' onClick={handleClick}>9</button>
-        <button className='botonColorido' name='*' onClick={handleClick}>*</button>
-        <button className='boton' name='4' onClick={handleClick}>4</button>
-        <button className='boton' name='5' onClick={handleClick}>5</button>
-        <button className='boton' name='6' onClick={handleClick}>6</button>
-        <button className='botonColorido' name='-' onClick={handleClick}>-</button>
-        <button className='boton' name='1' onClick={handleClick}>1</button>
-        <button className='boton' name='2' onClick={handleClick}>2</button>
-        <button className='boton' name='3' onClick={handleClick}>3</button>
-        <button className='botonColorido' name='+' onClick={handleClick}>+</button>
-        <button className='boton' name='0' onClick={handleClick}>0</button>
-        <button className='boton' name='.' onClick={handleClick}>.</button>
-        <button className='botonColorido' name='+/-' onClick={negativador}>+/-</button>
-        <button className='botonColorido' name='%' onClick={handleClick}>%</button>
-        <button className='especial2' onClick={calcularClick}>=</button>
+    <body>
+    <section className="calculator">
+      <div className="calculator__display">
+        <input
+          id="display-result"
+          className="calculator__display-result"
+          placeholder="0"
+          oninput="this.value = this.value.replace(/[^(0-9)(-/*+)]/g, '').replace(/(\..*?)\..*/g, '$1').replace(/^0[^.]/, '0');"
+          autofocus
+          autocomplete="off"
+        />
       </div>
-    </div>
+      <div className="calculator__keypad">
+        <div className="calculator__keypad-row">
+          <input
+            type="button"
+            id="button-clear"
+            name="button-clear"
+            className="calculator__keypad-row-button primary-button"
+            value="C"
+            onclick="clearCalculator()"
+          />
+          <input
+            type="button"
+            name="button-round"
+            className="calculator__keypad-row-button Advance"
+            value="r"
+            onclick="advanceCalculator('round')"
+          />
+          <input
+            type="button"
+            name="button-square"
+            className="calculator__keypad-row-button Advance"
+            value="&#8730;"
+            onclick="advanceCalculator('sqrt')"
+          />
+          <input
+            type="button"
+            name="button-absolute"
+            className="calculator__keypad-row-button Advance"
+            value="|x|"
+            onclick="advanceCalculator('abs')"
+          />
+        </div>
+        <div className="calculator__keypad-row">
+          <input
+            type="button"
+            name="button-seven"
+            className="calculator__keypad-row-button"
+            value="7"
+            onclick="addToCalculator(value)"
+          />
+          <input
+            type="button"
+            name="button-eight"
+            className="calculator__keypad-row-button"
+            value="8"
+            onclick="addToCalculator(value)"
+          />
+          <input
+            type="button"
+            name="button-nine"
+            className="calculator__keypad-row-button"
+            value="9"
+            onclick="addToCalculator(value)"
+          />
+          <input
+            type="button"
+            name="button-plus"
+            className="calculator__keypad-row-button"
+            value="+"
+            onclick="addToCalculator(value)"
+          />
+        </div>
+        <div className="calculator__keypad-row">
+          <input
+            type="button"
+            name="button-four"
+            className="calculator__keypad-row-button"
+            value="4"
+            onclick="addToCalculator(value)"
+          />
+          <input
+            type="button"
+            name="button-five"
+            className="calculator__keypad-row-button"
+            value="5"
+            onclick="addToCalculator(value)"
+          />
+          <input
+            type="button"
+            name="button-six"
+            className="calculator__keypad-row-button"
+            value="6"
+            onclick="addToCalculator(value)"
+          />
+          <input
+            type="button"
+            name="button-subtract"
+            className="calculator__keypad-row-button"
+            value="-"
+            onclick="addToCalculator(value)"
+          />
+        </div>
+        <div className="calculator__keypad-row">
+          <input
+            type="button"
+            name="button-one"
+            className="calculator__keypad-row-button"
+            value="1"
+            onclick="addToCalculator(value)"
+          />
+          <input
+            type="button"
+            name="button-two"
+            className="calculator__keypad-row-button"
+            value="2"
+            onclick="addToCalculator(value)"
+          />
+          <input
+            type="button"
+            name="button-three"
+            className="calculator__keypad-row-button"
+            value="3"
+            onclick="addToCalculator(value)"
+          />
+          <input
+            type="button"
+            name="button-multiply"
+            className="calculator__keypad-row-button"
+            value="×"
+            onclick="addToCalculator(value)"
+          />
+        </div>
+        <div className="calculator__keypad-row">
+          <input
+            type="button"
+            name="button-zero"
+            className="calculator__keypad-row-button"
+            value="0"
+            onclick="addToCalculator(value)"
+          />
+          <input
+            type="button"
+            name="button-dot"
+            className="calculator__keypad-row-button"
+            value="."
+            onclick="addToCalculator(value)"
+          />
+          <input
+            type="button"
+            name="button-divide"
+            className="calculator__keypad-row-button"
+            value="÷"
+            onclick="addToCalculator(value)"
+          />
+          <input
+            type="button"
+            name="button-equal"
+            id="button-equal"
+            className="calculator__keypad-row-button primary-button"
+            value="="
+            onclick="resultCalculator()"
+          />
+        </div>
+      </div>
+    </section>
+  </body>
   );
 }
 
